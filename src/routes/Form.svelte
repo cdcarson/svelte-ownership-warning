35159027c29fff5d10d053d1f708a3e62a7526fa
Christopher Carson <chris@nowzoo.com>
2024-04-14 10:01:21 -0500

    handler

--- src/routes/Form.svelte
+++ src/routes/Form.svelte
@@ -15,7 +15,7 @@
       name="name"
       id="name"
       bind:value={data.name}
-      on:blur={() => (touched.name = true)}
+      onblur={() => (touched.name = true)}
     />
   </div>
 </form>
