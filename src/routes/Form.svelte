b7be0fd0f73d43607e539bbad2516919fb374142
Christopher Carson <chris@nowzoo.com>
2024-04-14 09:39:24 -0500

    arghh tabs

--- src/routes/Form.svelte
+++ src/routes/Form.svelte
@@ -1,33 +1,33 @@
 <script lang="ts">
-	let data = $state({
-		firstName: '',
-		lastName: ''
-	});
-	let touched = $state({
-		firstName: false,
-		lastName: false
-	});
+  let data = $state({
+    firstName: '',
+    lastName: ''
+  });
+  let touched = $state({
+    firstName: false,
+    lastName: false
+  });
 </script>
 
 <form>
-	<div>
-		<label for="firstName">First Name</label>
-		<input
-			type="text"
-			name="firstName"
-			id="firstName"
-			bind:value={data.firstName}
-			on:blur={() => (touched.firstName = true)}
-		/>
-	</div>
   <div>
-		<label for="lastName">Last Name</label>
-		<input
-			type="text"
-			name="lastName"
-			id="lastName"
-			bind:value={data.lastName}
-			on:blur={() => (touched.lastName = true)}
-		/>
-	</div>
+    <label for="firstName">First Name</label>
+    <input
+      type="text"
+      name="firstName"
+      id="firstName"
+      bind:value={data.firstName}
+      on:blur={() => (touched.firstName = true)}
+    />
+  </div>
+  <div>
+    <label for="lastName">Last Name</label>
+    <input
+      type="text"
+      name="lastName"
+      id="lastName"
+      bind:value={data.lastName}
+      on:blur={() => (touched.lastName = true)}
+    />
+  </div>
 </form>
